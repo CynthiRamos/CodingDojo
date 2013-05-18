f0622414765d7a3d6f22892a134ea3c059e8be7e
Cynthi <Cynthi@10.230.170.85>
2013-05-18 11:50:52 +0100

    primer test

--- test/PalindromosTest.java
+++ test/PalindromosTest.java
@@ -1,10 +1,13 @@
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PalindromosTest {
     
     @Test
     public void PalindromosTest_unValor() {
-        
+        BuscadorPalindromos buscaPalindromo = new BuscadorPalindromos(1);
+        buscaPalindromo.isPalindromo();
+        Assert.assertTrue(buscaPalindromo.isPalindromo());
     }
 }
